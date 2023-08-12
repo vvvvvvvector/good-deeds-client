5dc95f40a0925601ff7661cf9dd8a78d5813ae42
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-08-12 23:34:07 +0300

    initial changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
+
+import { Toaster } from 'react-hot-toast';
+
+import '@/styles/globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const title = 'Good Things 🥺';
+
+  const mainstyle = `${inter.className} grid h-full w-full place-items-center`;
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <main className={mainstyle}>
+        <Component {...pageProps} />
+        <Toaster />
+      </main>
+    </>
+  );
 }
